910fbcdf54a48f7773af7a8387c00692a66b9427
gnocarroll <gocarrol@purdue.edu>
2025-03-29 03:31:21 -0700

    change how X MACRO used in TokenType.hpp

--- include/TokenType.hpp
+++ include/TokenType.hpp
@@ -3,66 +3,66 @@
 #include <cassert>
 #include <string_view>
 
-#define FOR_EACH_TOK(DO) \
-	DO(LEFT_PAREN) \
-	DO(RIGHT_PAREN) \
-	DO(LEFT_BRACE) \
-	DO(RIGHT_BRACE) \
-	DO(COMMA) \
-	DO(DOT) \
-	DO(MINUS) \
-	DO(PLUS) \
-	DO(SEMICOLON) \
-	DO(SLASH) \
-	DO(STAR) \
-	DO(BANG) \
-	DO(BANG_EQUAL) \
-	DO(EQUAL) \
-	DO(EQUAL_EQUAL) \
-	DO(GREATER) \
-	DO(GREATER_EQUAL) \
-	DO(LESS) \
-	DO(LESS_EQUAL) \
-	DO(IDENTIFIER) \
-	DO(STRING) \
-	DO(NUMBER) \
-	DO(AND) \
-	DO(CLASS) \
-	DO(ELSE) \
-	DO(FALSE) \
-	DO(FUN) \
-	DO(FOR) \
-	DO(IF) \
-	DO(NIL) \
-	DO(OR) \
-	DO(PRINT) \
-	DO(RETURN) \
-	DO(SUPER) \
-	DO(THIS) \
-	DO(TRUE) \
-	DO(VAR) \
-	DO(WHILE) \
-	DO(TOK_EOF) \
-	DO(LOX_TOKENTYPE_COUNT)
+#define INJECT_TOKS \
+	TOK(LEFT_PAREN) \
+	TOK(RIGHT_PAREN) \
+	TOK(LEFT_BRACE) \
+	TOK(RIGHT_BRACE) \
+	TOK(COMMA) \
+	TOK(DOT) \
+	TOK(MINUS) \
+	TOK(PLUS) \
+	TOK(SEMICOLON) \
+	TOK(SLASH) \
+	TOK(STAR) \
+	TOK(BANG) \
+	TOK(BANG_EQUAL) \
+	TOK(EQUAL) \
+	TOK(EQUAL_EQUAL) \
+	TOK(GREATER) \
+	TOK(GREATER_EQUAL) \
+	TOK(LESS) \
+	TOK(LESS_EQUAL) \
+	TOK(IDENTIFIER) \
+	TOK(STRING) \
+	TOK(NUMBER) \
+	TOK(AND) \
+	TOK(CLASS) \
+	TOK(ELSE) \
+	TOK(FALSE) \
+	TOK(FUN) \
+	TOK(FOR) \
+	TOK(IF) \
+	TOK(NIL) \
+	TOK(OR) \
+	TOK(PRINT) \
+	TOK(RETURN) \
+	TOK(SUPER) \
+	TOK(THIS) \
+	TOK(TRUE) \
+	TOK(VAR) \
+	TOK(WHILE) \
+	TOK(TOK_EOF) \
+	TOK(LOX_TOKENTYPE_COUNT)
 
 class TokenType {
 	static constexpr char *cStrs[] = {
-#define NAME_STR(name) (char * const) #name,
+#define TOK(name) (char * const) #name,
 
-		FOR_EACH_TOK(NAME_STR)
+		INJECT_TOKS
 
-#undef NAME_STR
+#undef TOK
 
 		(char * const) ""
 	};
 
 public:
 	enum Type {
-#define NAME_COMMA(name) name,
+#define TOK(name) name,
 
-		FOR_EACH_TOK(NAME_COMMA)
+		INJECT_TOKS
 
-#undef NAME_COMMA
+#undef TOK
 	};
 
 private:
@@ -88,4 +88,4 @@ public:
 	}
 };
 
-#undef FOR_EACH_TOK
+#undef INJECT_TOKS
